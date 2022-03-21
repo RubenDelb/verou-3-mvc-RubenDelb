04f2364c4c8cb3c7ab94feb0ad32a85980bec10f
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-03-21 11:10:41 +0100

    Create function to get all articles from database

--- controller/ArticleController.php
+++ controller/ArticleController.php
@@ -16,12 +16,18 @@ class ArticleController
     // Note: this function can also be used in a repository - the choice is yours
     private function getArticles()
     {
-        // TODO: prepare the database connection
+        // Prepare the database connection
         // Note: you might want to use a re-usable databaseManager class - the choice is yours
-        // TODO: fetch all articles as $rawArticles (as a simple array)
-        $rawArticles = [];
-
-        $articles = [];
+        require 'Model/DatabaseManager.php';
+        require 'config.php';
+        $databaseManager = new DatabaseManager($config['host'], $config['user'], $config['password'], $config['dbname']);
+        $databaseManager->connect();
+        // Fetch all articles as $rawArticles (as a simple array)
+        $query = "SELECT title, `description`, publish_date FROM articles";
+        $result = $databaseManager->connection->prepare($query);
+        $result->execute();
+        $rawArticles = $result->fetchAll(PDO::FETCH_ASSOC);
+        // $articles = [];
         foreach ($rawArticles as $rawArticle) {
             // We are converting an article from a "dumb" array to a much more flexible class
             $articles[] = new Article($rawArticle['title'], $rawArticle['description'], $rawArticle['publish_date']);
