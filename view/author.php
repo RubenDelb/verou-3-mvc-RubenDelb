460b0d781543b875f7ee81ed131f523393f395a8
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-03-22 10:39:55 +0100

    Create author.php to list all articles by one author

---
+++ view/author.php
@@ -0,0 +1,14 @@
+<?php require 'View/includes/header.php'?>
+
+<?php // Use any data loaded in the controller here ?>
+
+<section>
+    <h1>Articles written by <i><?= $_GET["author"] ?></i> </h1>
+    <ul>
+        <?php foreach ($articles as $article) : if ($article->author == $_GET["author"]){ ?>
+            <li><a href="index.php?page=articles-show&id=<?= $article->id ?>"><?= $article->title ?></a> (<?= $article->formatPublishDate() ?>)</li>
+        <?php } endforeach; ?>
+    </ul>
+</section>
+
+<?php require 'View/includes/footer.php'?>
